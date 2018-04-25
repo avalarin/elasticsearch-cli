2d8b958e8a774c48edfd152863a746d6b2c2cc1d
Prokopev Alexandr <a.prokopev@qiwi.com>
2018-04-25 21:59:27 +0300

    update ci scripts

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -13,26 +13,26 @@ pack() {
 
     tempdir=$(mktemp -d 2>/dev/null || mktemp -d -t tmp)
     out_dir=$(pwd)
-    package_name="$PROJECT_NAME-$TRAVIS_TAG-$TARGET"
+    package_name="$PROJECT_NAME-$TRAVIS_TAG-$SUFFIX"
 
     # create a "staging" directory
     mkdir "$tempdir/$package_name"
-    mkdir "$tempdir/$package_name/autocomplete"
-
+    
     # copying the main binary
     cp "target/$TARGET/release/$PROJECT_NAME" "$tempdir/$package_name/"
     strip "$tempdir/$package_name/$PROJECT_NAME"
 
     # manpage, readme and license
-    cp "doc/$PROJECT_NAME.1" "$tempdir/$package_name"
+    # cp "doc/$PROJECT_NAME.1" "$tempdir/$package_name"
     cp README.md "$tempdir/$package_name"
-    cp LICENSE-MIT "$tempdir/$package_name"
-    cp LICENSE-APACHE "$tempdir/$package_name"
+    # cp LICENSE-MIT "$tempdir/$package_name"
+    # cp LICENSE-APACHE "$tempdir/$package_name"
 
     # various autocomplete
-    cp target/"$TARGET"/release/build/"$PROJECT_NAME"-*/out/"$PROJECT_NAME".bash "$tempdir/$package_name/autocomplete/${PROJECT_NAME}.bash-completion"
-    cp target/"$TARGET"/release/build/"$PROJECT_NAME"-*/out/"$PROJECT_NAME".fish "$tempdir/$package_name/autocomplete"
-    cp target/"$TARGET"/release/build/"$PROJECT_NAME"-*/out/_"$PROJECT_NAME" "$tempdir/$package_name/autocomplete"
+    # mkdir "$tempdir/$package_name/autocomplete"
+    # cp target/"$TARGET"/release/build/"$PROJECT_NAME"-*/out/"$PROJECT_NAME".bash "$tempdir/$package_name/autocomplete/${PROJECT_NAME}.bash-completion"
+    # cp target/"$TARGET"/release/build/"$PROJECT_NAME"-*/out/"$PROJECT_NAME".fish "$tempdir/$package_name/autocomplete"
+    # cp target/"$TARGET"/release/build/"$PROJECT_NAME"-*/out/_"$PROJECT_NAME" "$tempdir/$package_name/autocomplete"
 
     # archiving
     pushd "$tempdir"
